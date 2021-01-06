e1595ae7635e6486f5fa02c31c2e68aa50fdd20b
Michael <michael@mikansystems.com>
2021-01-06 00:28:34 -0800

    fix token variable

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ certificateName="$(echo $RENEWED_LINEAGE | sed 's/.*\///')"
 
 searchUrl="$($netbox_url)virtualization/virtual-machines/?tag=$($netbox_tag_base)$($certificateName)"
 
-curl -sS -H "Authorization: token d4cf122bc7fa6f16c6211d47ccd7f9363760a441" $searchUrl | jq -r '.results[] .primary_ip4.address' | sed "s/\/.*//" | while read ip_address; do
+curl -sS -H "Authorization: token $netbox_token" $searchUrl | jq -r '.results[] .primary_ip4.address' | sed "s/\/.*//" | while read ip_address; do
     scp $RENEWED_LINEAGE/fullchain.pem -i $ssh_key $ssh_username@$ip_address:$dest_path
     scp $RENEWED_LINEAGE/privkey.pem -i $ssh_key $ssh_username@$ip_address:$dest_path
 done
